89928eac07f54558bd8f56af58f0171d6e79ee96
Nikolaj Veljkovic <nikolaj.veljkovic@mygymer.ch>
2024-03-02 18:27:20 +0100

    Updated sort-vinyls.py

--- sort-vinyls.py
+++ sort-vinyls.py
@@ -1,32 +1,43 @@
 from re import findall
 import string
 
+original_list = []
+last_name_list = []
 alphabet = string.ascii_uppercase
 # regex pattern
 pattern = ",(\s[A-Za-z0-9]+\s?)?\s([A-Za-z0-9]+)"
 
-def create_lists():
-    
-def add_album(vinyl):
-    
-    # loop through list
+
+def create_arrays():
+    # loop through vinyls.txt
     book = open("vinyls.txt", "r")
     for item in book:
+         
+        # findall returns two dimensional list (theoretically tuple inside of list)
+        # [0][1] finds second item (first might be empty or not)
+        # (the CROOKS, michael JACKSON, QUEEN)
+        last_name = findall(pattern, item)[0][1]
         
-        if item == vinyl:
-            print("You have already added this album.")
-            
-        else:
-            # findall returns two dimensional list (theoretically tuple inside of list)
-            # [0][1] finds second item (first might be empty or not)
-            name = findall(pattern, item)[0][1]
+        original_list.append(item)
+        last_name_list.append(last_name)
         
-            # look at last names, alphabetically, loop through first letters (get index from alphabet)
+def search_index(new_last_name):
+
+    for i in range(len(last_names_list)):
+        
+        if alphabet.index(new_last_name[0].capitalize()) == alphabet.index(last_names_list[i][0].capitalize()):
+            search_index(new_last_name[1:0])
             
-            # insert into list
+        else if alphabet.index(new_last_name[0].capitalize()) < alphabet.index(last_names_list[i][0].capitalize()):
+            continue
+        
+        else if alphabet.index(new_last_name[0].capitalize()) > alphabet.index(last_names_list[i][0].capitalize()):
+            # insert method
 
 print("Hi. Please type in the required names. Make sure to write the artist's name as [first_name last_name].")
 # get input from user (ask for album title, artist/band name)
 # vinyl = input("[album name], [artist/band name]: ")
 album_and_artist = "coco, channel"
-find_album(album_and_artist)
+new_last_name = findall(pattern, album_and_artist)
+search_index(new_last_name)
+create_arrays()
