8ee0c11783c0091d878eba926683fd539a2181a6
Nikolaj Veljkovic <nikolaj.veljkovic@mygymer.ch>
2024-03-07 21:50:56 +0100

    Simple inputting items and (inbuilt) sorting.

--- sort-vinyls.py
+++ sort-vinyls.py
@@ -1,49 +1,36 @@
-from re import findall
+# from re import findall
 
-class SortVinyls():
+data_base = []
+filename = "vinyls.txt"
+new_input = ""
+
+def load_db():    
+    file = open(filename, "r")
+    for line in file:
+        line = line.strip()
+        if line != "":
+            data_base.append(line)
+    file.close()
+
+def sort_db():
+    data_base.sort()
+
+def add_to_db(item):
+    data_base.append(item)
     
-    def __init__(self, filename):
-        self.filename = filename
-        self.data_base = []
-        self.load_data_base()
+def out_to_file():
+    sort_db()
     
-    def load_data_base(self):
-        file = open(self.filename, "r")
-        for line in file:
-            file.strip()
-            self.data_base.append(line)
-        file.close()
-        self.data_base.sort()
-    
-    def add_vinyl(self, new_vinyl, old_vinyl):
-        
-        for i in range(len(self.original_list)):
-            if new_vinyl[0] == original_list
-    
-class Vinyl():
-    
-    def __init__(self, vinyl):
-        self.vinyl = vinyl.upper()
-    
-    def __eq__(self, other):
-        return self.vinyl == other.vinyl
-    
-    def __str__(self):
-        return self.vinyl
-    
-    def compare(self, other):
-        len_vinyl = len(self.vinyl)
-        
-        for i, name in enumerate(len_vinyl):
-            if self.vinyl[i] == other.vinyl[i]:
-                
-        
-v = SortVinyls()
-print(v.original_list)
-# take name of album from list
-# if the first letter of old album is smaller in ascii
-    # insert before
-# elif the first letter of old album is bigger
-    # go to next album
-# else (the letters are equal)
-    # go back to step two, take away the first letters
+    file = open(filename, "w")
+    for line in data_base:
+        line += "\n"
+        file.write(line)
+    file.close()
+
+load_db()
+new_input = input("Type STOP to stop adding.\nWhat do you wanna add? ")
+while new_input != "STOP":
+    add_to_db(new_input)
+    out_to_file()
+    new_input = input("\nType STOP to stop adding.\nWhat do you wanna add? ")
+print("That was fun! Your items are safe with me.")
